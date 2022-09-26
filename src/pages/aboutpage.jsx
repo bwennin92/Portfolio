ea85ac0fa86b92f7ca3b455b6aa2c044831ed485
Brandon! <brandonmw1@hotmail.com>
2022-09-25 20:12:48 -0700

    still working on text

--- src/pages/aboutpage.jsx
+++ src/pages/aboutpage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='bg-black, text-green-700'>
-        <h1>About me!</h1>
-        <p>Hi! My name is Brandon and I like to code. I started to code in March and fell in love with it.</p>
-        <br />
-        <p>It has been what seems like a long journey when I began my course with General Assembly and I have learned so much. From learning Javascript, React, Node.js,Python, MongoDB, SQL and Django.</p>
-        <p>Come follow me on my journey to being a software developer!</p>
+    <div className='bg-black'>
+        <div className=' text-green-700'>
+          <h1>About me!</h1>
+          <p>Hi! My name is Brandon and I like to code. I started to code in March and fell in love with it.</p>
+          <br />
+          <p>It has been what seems like a long journey when I began my course with General Assembly and I have learned so much. From learning Javascript, React, Node.js,Python, MongoDB, SQL and Django.</p>
+          <p>Come follow me on my journey to being a software developer!</p>
+        </div>
     </div>
   )
 }
