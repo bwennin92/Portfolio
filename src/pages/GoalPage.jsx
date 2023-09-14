579bc9228178a166371719a478a26e66b372b2a4
bwennin92 <bwennin92@gmail.com>
2023-09-13 17:56:47 -0700

    finally made the background work

--- src/pages/GoalPage.jsx
+++ src/pages/GoalPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import GoalComp from "../components/GoalComp";
 import '../components/goalCss.css'
+import background from "../Pictures/bmwsupra92_a_computer_placed_on_a_desk.png"
 const GoalPage = () => {
   return (
     <div className="background_container"
       style={{
-        backgroundImage: "url(/static/media/bmwsupra92_a_computer_placed_on_a_desk.85ab27dba9f98b5ffba1.png)",
+        backgroundImage: `url(${background})`,
       }}
     >
       <GoalComp />
